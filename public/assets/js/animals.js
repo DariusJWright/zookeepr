b1adb86e39925b0c10cbb5109bdc2a9ff3ccc6a6
darshnablah <dwright758@gmail.com>
2020-07-31 13:39:45 -0500

    heroku error fix

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -2,7 +2,7 @@ const $animalForm = document.querySelector('#animals-form');
 const $displayArea = document.querySelector('#display-area');
 
 const printResults = resultArr => {
-  console.log(resultArr);
+  console.log('RESULTARR---> ',resultArr);
 
   const animalHTML = resultArr.map(({ id, name, personalityTraits, species, diet }) => {
     return `
